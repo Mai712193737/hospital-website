02dd7b68fd535242ec50a62826521e5c3695e929
bishoDev123 <bisholiveenglish@gmail.com>
2025-06-24 22:07:11 +0300

    updated the contact us page to match the rest of the website

--- temp/Doctors/doctors.js
+++
@@ -1,7 +0,0 @@
-let btn = document.querySelectorAll(".book-btn");
-
-btn.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    alert("Appointment booked with the doctor ✅");
-  });
-});
